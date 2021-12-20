1d41a714b8a0c058907d170058a42bd247c91d34
Ben McLean <bmcle071@uottawa.ca>
2021-12-20 11:32:18 -0500

    Auto formatting

--- day-1/solution.py
+++ day-1/solution.py
@@ -2,21 +2,24 @@ import os
 from typing import List, Union
 from enum import Enum
 
+
 class Step(Enum):
     UP = 1
     DOWN = 2
     NONE = 3
     NA = 4
 
+
 def read_nums(filename="input.txt") -> List[int]:
     dirname = os.path.dirname(__file__)
     with open(os.path.join(dirname, filename), 'r') as f:
         return [int(line) for line in f.readlines()]
 
+
 def find_step(prev: Union[None, int], curr: int) -> Step:
     if prev == None:
         return Step.NA
-    
+
     elif (curr < prev):
         return Step.DOWN
     elif (curr > prev):
@@ -24,16 +27,20 @@ def find_step(prev: Union[None, int], curr: int) -> Step:
     else:
         return Step.NONE
 
+
 def sliding_window_sum(nums: List[int], size=3) -> List[int]:
     return [sum(nums[i:i+size]) for i in range(len(nums) - size + 1)]
+
 
 def map_steps(nums: List[int]) -> List[Step]:
     l = [None, *nums]
 
-    return [find_step(p, c) for p, c in zip(l, l[1:])] 
+    return [find_step(p, c) for p, c in zip(l, l[1:])]
+
 
 def count_step_ups(steps: List[Step]) -> int:
     return sum([1 for step in steps if step == Step.UP])
+
 
 if __name__ == "__main__":
     depths = read_nums()
