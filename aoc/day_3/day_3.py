afc1a386d79906494111770a0df532419079c0c6
Ben McLean <bmcle071@uottawa.ca>
2021-12-25 12:36:23 -0500

    Solving day 3

--- aoc/day_3/day_3.py
+++ aoc/day_3/day_3.py
@@ -1,5 +1,14 @@
 from typing import List
 import os
+from decimal import Decimal
+import math
+
+
+def col_round(x):  # round to nearest, tie breaker rounds up
+    frac = x - math.floor(x)
+    if frac < 0.5:
+        return math.floor(x)
+    return math.ceil(x)
 
 
 def read_nytes(filename="input.txt") -> List[bool]:
@@ -16,7 +25,7 @@ def get_average_ith_place(nytes: List[List[bool]], i: int) -> float:
 def get_gamma_rate(nytes: List[List[bool]]) -> List[bool]:
     size = len(nytes[0])
 
-    common = [round(get_average_ith_place(nytes, i)) == 1 for i in range(size)]
+    common = [get_most_common_ith(nytes, i) for i in range(size)]
 
     return common
 
@@ -24,3 +33,57 @@ def get_gamma_rate(nytes: List[List[bool]]) -> List[bool]:
 def get_epsilon_rate(nytes: List[List[bool]]) -> List[bool]:
     gamma_rate = get_gamma_rate(nytes)
     return [not g for g in gamma_rate]
+
+
+def all_same(nytes: List[List[bool]]) -> bool:
+    first = nytes[0]
+    return all([nyte == first for nyte in nytes])
+
+
+def get_most_common_ith(nytes: List[List[bool]], place) -> bool:
+    return col_round(get_average_ith_place(nytes, place)) == 1
+
+
+def place_equal(nyte: List[bool], bit: bool, place: int) -> bool:
+    return nyte[place] == bit
+
+
+def get_oxygen_generator_rating(nytes: List[List[bool]], place=0) -> int:
+    if len(nytes) == 1 or all_same(nytes):
+        return nytes[0]
+    else:
+        c = get_most_common_ith(nytes, place)
+        m = [n for n in nytes if place_equal(nyte=n, bit=c, place=place)]
+        return get_oxygen_generator_rating(m, place + 1)
+
+
+def get_c02_scrubber_rating(nytes: List[List[bool]], place=0) -> int:
+    if len(nytes) == 1 or all_same(nytes):
+        return nytes[0]
+    else:
+        most_common = get_most_common_ith(nytes, place)
+        matching = [n for n in nytes if not place_equal(n, most_common, place)]
+        return get_c02_scrubber_rating(matching, place + 1)
+
+
+def to_decimal(nytes: List[bool]) -> int:
+    return sum([2**i * n for i, n in enumerate(nytes[::-1])])
+
+
+def get_part_one(nytes: List[List[bool]]) -> int:
+    gamma = to_decimal(get_gamma_rate(nytes))
+    epsilon = to_decimal(get_epsilon_rate(nytes))
+    return gamma * epsilon
+
+
+def get_part_two(nytes: List[List[bool]]) -> int:
+    oxygen = to_decimal(get_oxygen_generator_rating(nytes))
+    c02 = to_decimal(get_c02_scrubber_rating(nytes))
+    return oxygen * c02
+
+
+if __name__ == "__main__":
+    nytes = read_nytes()
+
+    print(f"Part 1 gamma * epsilon: {get_part_one(nytes)}")
+    print(f"Part 2 02 * C02: {get_part_two(nytes)}")
--- aoc/day_3/test_day_3.py
+++ aoc/day_3/test_day_3.py
@@ -1,8 +1,30 @@
-from aoc.day_3.day_3 import get_average_ith_place, read_nytes, get_gamma_rate, get_epsilon_rate, get_average_ith_place
+import aoc.day_3.day_3 as day_3
+
+
+def test_place_equal():
+    assert day_3.place_equal(nyte=[1, 0, 1], bit=0, place=0) == False
+    assert day_3.place_equal(nyte=[1, 0, 1], bit=1, place=0) == True
+    assert day_3.place_equal(nyte=[1, 0, 1], bit=0, place=1) == True
+    assert day_3.place_equal(nyte=[1, 0, 1], bit=1, place=1) == False
+    assert day_3.place_equal(nyte=[1, 0, 1], bit=0, place=2) == False
+    assert day_3.place_equal(nyte=[1, 0, 1], bit=1, place=2) == True
+
+
+def test_col_round():
+    assert day_3.col_round(0) == 0
+    assert day_3.col_round(0.2) == 0
+    assert day_3.col_round(0.5) == 1
+    assert day_3.col_round(1.1) == 1
+    assert day_3.col_round(1.5) == 2
+    assert day_3.col_round(-0) == 0
+    assert day_3.col_round(-0.2) == 0
+    assert day_3.col_round(-0.5) == 0
+    assert day_3.col_round(-1.1) == -1
+    assert day_3.col_round(-1.5) == -1
 
 
 def test_read_nytes():
-    nytes = read_nytes("test.txt")
+    nytes = day_3.read_nytes("test.txt")
 
     assert len(nytes) > 3
     assert nytes[0] == [0, 0, 1, 0, 0]
@@ -11,20 +33,58 @@ def test_read_nytes():
 
 
 def test_get_gamma_rate():
-    nytes = read_nytes("test.txt")
+    nytes = day_3.read_nytes("test.txt")
 
-    assert get_gamma_rate(nytes) == [1, 0, 1, 1, 0]
+    assert day_3.get_gamma_rate(nytes) == [1, 0, 1, 1, 0]
 
 
 def test_get_epsilon_rate():
-    nytes = read_nytes("test.txt")
-    assert get_epsilon_rate(nytes) == [0, 1, 0, 0, 1]
+    nytes = day_3.read_nytes("test.txt")
+    assert day_3.get_epsilon_rate(nytes) == [0, 1, 0, 0, 1]
 
 
 def test_get_average_ith_place():
-    nytes = read_nytes("test.txt")
-    assert get_average_ith_place(nytes, 0) == 7 / 12
-    assert get_average_ith_place(nytes, 1) == 5 / 12
-    assert get_average_ith_place(nytes, 2) == 8 / 12
-    assert get_average_ith_place(nytes, 3) == 7 / 12
-    assert get_average_ith_place(nytes, 4) == 5 / 12
+    nytes = day_3.read_nytes("test.txt")
+    assert day_3.get_average_ith_place(nytes, 0) == 7 / 12
+    assert day_3.get_average_ith_place(nytes, 1) == 5 / 12
+    assert day_3.get_average_ith_place(nytes, 2) == 8 / 12
+    assert day_3.get_average_ith_place(nytes, 3) == 7 / 12
+    assert day_3.get_average_ith_place(nytes, 4) == 5 / 12
+
+
+def test_to_decimal():
+    nytes = [1, 1, 0, 1, 0]
+    assert day_3.to_decimal(nytes) == 26
+
+
+def test_all_same():
+    assert day_3.all_same([[1, 1, 1, 1, 0], [1, 1, 1, 1, 1]]) == False
+    assert day_3.all_same([[1, 1, 1, 1, 0], [1, 1, 1, 1, 0]]) == True
+
+
+def test_get_most_common_ith():
+    nytes = [[1, 0, 1], [1, 1, 0], [0, 0, 1]]
+    assert day_3.get_most_common_ith(nytes, 0) == 1
+    assert day_3.get_most_common_ith(nytes, 1) == 0
+    assert day_3.get_most_common_ith(nytes, 2) == 1
+    assert day_3.get_most_common_ith([[0], [1]], 0) == 1
+
+
+def test_get_oxygen_generator_rating():
+    nytes = day_3.read_nytes("test.txt")
+    assert day_3.get_oxygen_generator_rating(nytes) == [1, 0, 1, 1, 1]
+
+
+def test_get_c02_scrubber_rating():
+    nytes = day_3.read_nytes("test.txt")
+    assert day_3.get_c02_scrubber_rating(nytes) == [0, 1, 0, 1, 0]
+
+
+def test_get_part_one():
+    nytes = day_3.read_nytes("test.txt")
+    assert day_3.get_part_one(nytes) == 198
+
+
+def test_get_part_two():
+    nytes = day_3.read_nytes("test.txt")
+    assert day_3.get_part_two(nytes) == 230
